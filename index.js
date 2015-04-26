f71f1f7d82889c933a186e6093ee26838c59179e
Ben Newman <bn@cs.stanford.edu>
2015-04-26 15:54:00 -0400

    Load the right (Debug) node-gyp module.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var offgrid = require('./build/Release/offgrid');
+var offgrid = require('./build/Debug/offgrid');
 
 // Selectively re-export and/or wrap offgrid methods.
 exports.blank = offgrid.blank;
+exports.red = offgrid.red;
+exports.green = offgrid.green;
 exports.blue = offgrid.blue;
 exports.write = offgrid.write;
 exports.send = offgrid.send;
