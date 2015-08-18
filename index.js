54791021017580fa0d843fba7aeba8eb72a0be4d
Ben Newman <bn@cs.stanford.edu>
2015-08-18 02:00:01 +0000

    Set LED count to 575.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var offgrid = require('./build/Debug/offgrid');
 
+offgrid.init(575);
+
 // Selectively re-export and/or wrap offgrid methods.
 exports.blank = offgrid.blank;
 exports.red = offgrid.red;
